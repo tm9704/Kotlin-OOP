54c0a13a26adc316c7769b6bf1348acebc7c861f
tm9704 <tm9704@korea.ac.kr>
2022-08-11 16:55:48 +0900

    Kotlin: Interface

--- app/src/main/java/com/tm9704/kotlinoop/InterfaceEx.kt
+++ app/src/main/java/com/tm9704/kotlinoop/InterfaceEx.kt
@@ -6,8 +6,16 @@ package com.tm9704.kotlinoop
 // inherited is called the Super class or Parent class
 // or Base class.
 
+interface Drivable{
+    val maxSpeed: Double
+    fun drive(): String
+    fun brake(){
+        println("The drivable is braking")
+    }
+}
+
 // Super Class, Parent Class, Base Class of Car
-/*open class Car(val name: String, val brand: String){
+open class Car(override val maxSpeed: Double,val name: String, val brand: String): Drivable{
     open var range: Double = 0.0
 
     fun extendRange(amount: Double){
@@ -15,13 +23,16 @@ package com.tm9704.kotlinoop
             range += amount
     }
 
+    override fun drive(): String = "driving the interface drive"
+
     open fun drive(distance: Double){
         println("Drove for $distance KM")
     }
 }
 
 // SubClass Child class or Derived class of Car
-class ElectricCar(name: String, brand: String, batteryLife: Double) : Car(name, brand){
+class ElectricCar(maxSpeed: Double,name: String, brand: String, batteryLife: Double)
+    : Car(maxSpeed,name, brand){
 
     var chargerType = "Type1"
     override var range = batteryLife * 5
@@ -30,22 +41,31 @@ class ElectricCar(name: String, brand: String, batteryLife: Double) : Car(name, 
         println("Drove for $distance KM on electricity")
     }
 
-    fun drive(){
-        println("Drove for $range KM on electricity")
+    override fun drive(): String{
+        return "Drove for $range KM on electricity"
+    }
+
+    override fun brake(){
+        super.brake()
+        println("brake inside of electric car")
     }
 }
 
 //Any class inherits from the Any Class
 
 fun main(){
-    var audiA3 = Car("A3", "Audi")
-    var teslaS = ElectricCar("S-Model", "Tesla", 85.0)
+    var audiA3 = Car(200.0,"A3", "Audi")
+    var teslaS = ElectricCar(240.0,"S-Model", "Tesla", 85.0)
 
     teslaS.chargerType = "Type2"
     teslaS.extendRange(200.0)
+
     teslaS.drive()
+    teslaS.brake()
+    audiA3.brake()
+
 
     //Polymorphism
     audiA3.drive(200.0)
     teslaS.drive(200.0)
-}*/
+}
